51d954c758d4a1149c39f3893c6c3adb0e07dce7
Diego da Silva Santana <diegosantanasantos@yahoo.com.br>
2023-07-10 23:18:31 -0300

    Cr desenvolvimento da classe config / User repository

---
+++ src/main/java/com/educandoWeb/course/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.educandoWeb.course.repositories;
+
+import com.educandoWeb.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
