b7830c7c5f3446c177f0efafc7efc1d5dba55c03
Diego da Silva Santana <diegosantanasantos@yahoo.com.br>
2023-07-14 08:17:26 -0300

    Desenvolvimento do Enum

--- src/main/java/com/educandoWeb/course/entities/Order.java
+++ src/main/java/com/educandoWeb/course/entities/Order.java
@@ -1,5 +1,7 @@
 package com.educandoWeb.course.entities;
 
+import com.educandoWeb.course.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -13,7 +15,11 @@ public class Order implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Instant moment;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
+          private Instant moment;
+
+        private Integer orderStatus;
         @ManyToOne
         @JoinColumn(name = "client_id")
         private User client;
@@ -21,9 +27,10 @@ public class Order implements Serializable{
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -49,6 +56,15 @@ public class Order implements Serializable{
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null)
+        this.orderStatus = orderStatus.getCode();
     }
 
     @Override
